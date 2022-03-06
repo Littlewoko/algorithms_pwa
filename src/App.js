c8dcfd100b85996c09b1c67d1c6d44a466631568
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-03-06 15:40:42 +0000

    working PWA skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-function App() {
+const Users = () => {
+  // state
+  const [users, setUsers] = React.useState([]);
+  // effects
+  React.useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => {
+        setUsers(users);
+      })
+      .catch((err) => {});
+  }, []);
+  //render
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Users</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.email})
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" exact element={<Users />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
