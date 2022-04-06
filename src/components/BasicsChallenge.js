4d752f7c8167f32c7e889f96b844729af39ae392
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-04-06 18:02:53 +0000

    Update BasicsChallenge.js

--- src/components/BasicsChallenge.js
+++ src/components/BasicsChallenge.js
@@ -9,22 +9,32 @@ class ChallengeForm extends React.Component {
             option_one : false,
             option_two : false,
             option_three : false,
-            options : ["option1", "option2", "option3"],
+            options : ["1", "2", "3"],
         }
     }
 
+    handleOption1Change = (e) => {
+        if (e.target.value === "1") {
+            this.setState({
+                option_one:true,
+            })
+        } else {
+            this.setState({
+                option_one:false,
+            })
+        }
+    }
 
     render() {
         return (
             <div>
                 <h1>Challenge</h1>
                 <label for="one">1.</label>
-                <select name="one" id="one">
-                    {this.state.options.map((x) => {
-                        return '<option value={x}>' + x + '</option>';
-                    })}
+                <select name="one" id="one" onChange={this.handleOption1Change}>
+                    <option value="option one">Option One</option>
+                    {this.state.options.map((option) => 
+                        <option value={option}>{option}</option>)}
                 </select>
-
             </div>
         );
     }
